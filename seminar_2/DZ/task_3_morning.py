696901d4dd2a3058264d822971a132d1e093f7b7
Vasiliy Maletin <108918665+VasiliyMaletin@users.noreply.github.com>
2022-10-19 22:11:40 +0800

    edit task_3_morning.py

--- seminar_2/DZ/task_3_morning.py
+++ seminar_2/DZ/task_3_morning.py
@@ -1,14 +1,24 @@
 # Напишите программу, в которой пользователь будет задавать две строки,
 # а программа - определять количество вхождений одной строки в другой.
 
+def create_string_list(number):
+    list = []
+    for i in range(number):
+        list.append(input(f'Введите {i + 1}-й элемент строки: '))
+    return list
+
 def check_string(str_1, str_2):
     count = 0
-    for i in str_2:
-        for j in str_1:
-            if j == i:
-                count += 1
+    i = -1
+    for i in str_1:
+        if str_2 == i:
+            count += 1
     return count
 
-str_1 = input("Введите первую строку: ")
-str_2 = input("Введите вторую строку: ")
-print(f"Количество вхождений второй строки в первую = {check_string(str_1, str_2)}")
+size = int(input('Введите количество элементов строки: '))
+str_1 = create_string_list(size)
+str_2 = input('Введите вторую строку: ')
+if check_string(str_1, str_2) == (-1):
+    print('Нет повторов')
+else:
+    print(f"Количество вхождений второй строки в первую = {check_string(str_1, str_2)}")
