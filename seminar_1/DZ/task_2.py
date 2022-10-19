6beb1ee4ca88653f3ffb5e97e982a0afdea09edf
Vasiliy Maletin <maletinvasilii@mail.ru>
2022-10-19 19:15:51 +0800

    completed dz seminar_1

---
+++ seminar_1/DZ/task_2.py
@@ -0,0 +1,15 @@
+# Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+flag = True
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            not (x or y or z) == (not x and not y and not z)
+            print(x, y, z, not (x or y or z) == (not x and not y and not z))
+            if not (x or y or z) != (not x and not y and not z):
+                flag = False
+if flag:
+    print("Утверждение истинно")
+else:
+    print("Утверждение ложно")
---
+++ seminar_1/DZ/task_5.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая принимает на вход координаты двух точек
+# и находит расстояние между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+try:
+    from math import sqrt
+    print("Введите координаты точки A:")
+    point_a_x = float(input("x = "))
+    point_a_y = float(input("y = "))
+    print("Введите координаты точки B:")
+    point_b_x = float(input("x = "))
+    point_b_y = float(input("y = "))
+    print("Расстояние между точкой A и B =", round(
+        sqrt((point_a_x - point_b_x)**2 + (point_a_y - point_b_y)**2), 2))
+except ValueError:
+    print("Нужно вводить числа!!!")
