fc86970b2ebff0449465249d2d5f96c20584d947
Vasiliy Maletin <108918665+VasiliyMaletin@users.noreply.github.com>
2022-10-20 12:08:43 +0800

    edited task_3_morning.py

--- seminar_2/DZ/task_3_morning.py
+++ seminar_2/DZ/task_3_morning.py
@@ -1,20 +1,22 @@
 # Напишите программу, в которой пользователь будет задавать две строки,
 # а программа - определять количество вхождений одной строки в другой.
 
-def create_string_list(number):
-    list = []
-    for i in range(number):
-        list.append(input(f"Введите {i + 1}й элемент строки: "))
-    return list
-
-def check_string(str_1, str_2):
+def checindex_string(str_1, str_2):
     count = 0
-    for i in str_1:
-        if str_2 == i:
-            count += 1
-    return count
+    res = 0
+    index = 0
+    for i in range(len(str_1)):
+        if str_1[i] == str_2[0]:
+            index = i + 1
+            for j in range(1, len(str_2)):
+                if index != len(str_1) and str_1[index] == str_2[j]:
+                    count += 1
+                    index += 1
+            if count == (len(str_2) - 1):
+                res += 1
+            count = 0
+    return res
 
-size = int(input("Введите количество элементов строки: "))
-str_1 = create_string_list(size)
+str_1 = input("Введите первую строку: ")
 str_2 = input("Введите вторую строку: ")
-print(f"Количество вхождений второй строки в первую = {check_string(str_1, str_2)}")
+print(f"Количество вхождений второй строки в первую = {checindex_string(str_1, str_2)}")
